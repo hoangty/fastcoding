549a196760f46f5e211148c47c727e245ab46dca
Hoang Ty <tyhoang@caters.co.jp>
2023-09-21 10:36:50 +0700

    udate public

--- _develop/npm_scripts/pug_func.js
+++
@@ -1,6 +0,0 @@
-{
-  nodeFs : require('fs'),
-  nodeDest : path.join(process.cwd(), process.env.npm_package_config_dest + process.env.npm_package_config_subDirectory),
-  nodePath : require('path'),
-  nodeSize : require('image-size')
-}
